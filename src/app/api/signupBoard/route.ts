74956ddc5e376ecafbfa1d2498f4bf61fb4a6351
SheikhJack <kealebogakido@gmail.com>
2025-01-27 12:23:25 +0200

    routes

--- src/app/api/signupBoard/route.ts
+++ src/app/api/signupBoard/route.ts
@@ -3,7 +3,7 @@ import pool from '../../../../lib/db'
 import { ResultSetHeader } from 'mysql2';
 
 
-export async function POST(req: Response) {
+export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { name, email, password } = body;
